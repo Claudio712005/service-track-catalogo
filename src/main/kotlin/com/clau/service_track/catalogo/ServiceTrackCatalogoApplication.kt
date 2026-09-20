a3e2f7c3ea3f98763e54a5b6450cf01cd3aed6eb
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-09-20 17:08:47 -0300

    Commit inicial

---
+++ src/main/kotlin/com/clau/service_track/catalogo/ServiceTrackCatalogoApplication.kt
@@ -0,0 +1,11 @@
+package com.clau.service_track.catalogo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceTrackCatalogoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceTrackCatalogoApplication>(*args)
+}
---
+++ src/test/kotlin/com/clau/service_track/catalogo/ServiceTrackCatalogoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clau.service_track.catalogo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceTrackCatalogoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
